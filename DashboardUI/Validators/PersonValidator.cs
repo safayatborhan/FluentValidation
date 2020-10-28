f0cb510fa59576cc8872f7efbd714655a6be05dc
safayatzisan <zisan_cse_mbstu@hotmail.com>
2020-10-28 12:34:10 +0600

    first commit

---
+++ DashboardUI/Validators/PersonValidator.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation;
+using ModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://fluentvalidation.net/
+
+namespace DashboardUI.Validators
+{
+  public class PersonValidator : AbstractValidator<PersonModel>
+  {
+    public PersonValidator()
+    {
+      RuleFor(p => p.FirstName)
+        .Cascade(CascadeMode.StopOnFirstFailure)
+        .NotEmpty().WithMessage("{PropertyName} is Empty")
+        .Length(2, 50).WithMessage("Length ({TotalLength}) of {PropertyName} Invalid")
+        .Must(BeAValidName).WithMessage("{PropertyName} Contains Invalid Characters");
+
+      RuleFor(p => p.LastName)
+        .Cascade(CascadeMode.StopOnFirstFailure)
+        .NotEmpty().WithMessage("{PropertyName} is Empty")
+        .Length(2, 50).WithMessage("Length ({TotalLength}) of {PropertyName} Invalid")
+        .Must(BeAValidName).WithMessage("{PropertyName} Contains Invalid Characters");
+
+      RuleFor(p => p.DateOfBirth)
+        .Must(BeAValidAge).WithMessage("Invalid {PropertyName}");
+    }
+
+    protected bool BeAValidName(string name)
+    {
+      name = name.Replace(" ", "");
+      name = name.Replace("-", "");
+      return name.All(Char.IsLetter);
+    }
+
+    protected bool BeAValidAge(DateTime date)
+    {
+      int currentYear = DateTime.Now.Year;
+      int dobYear = date.Year;
+
+      if(dobYear <= currentYear && dobYear > (currentYear - 120))
+      {
+        return true;
+      }
+
+      return false;
+    }
+  }
+}
